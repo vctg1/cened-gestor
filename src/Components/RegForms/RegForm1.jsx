e65c3e93f9c1e94e65f7c88d159e9e344acb020c
vctg1 <victormourago@gmail.com>
2022-07-13 15:57:15 -0300

    'add-calendar'

--- src/Components/RegForms/RegForm1.jsx
+++ src/Components/RegForms/RegForm1.jsx
@@ -1,10 +1,13 @@
-import { Box, MenuItem, TextField } from '@mui/material';
-import React from 'react';
+import { Box, Grid, MenuItem, TextField } from '@mui/material';
+import React, {useState} from 'react';
 import InputMask from 'react-input-mask';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
 
 
 export default function RegForm1({data, setData, muiAlert}){
-    let formatCharsYear = {'Y': '[0-9]','m': '[0-1]','M': '[0-9]','d': '[0-3]','D': '[0-9]'};
     let uf = [{value:1,txt:'Acre'}, 
     {value:2, txt:'Alagoas'}, {value:3, txt:'Amapá'}, 
     {value:4, txt:'Amazonas'}, {value:5, txt:'Bahia'}, 
@@ -20,12 +23,20 @@ export default function RegForm1({data, setData, muiAlert}){
     {value:27,txt:'Tocantins'}];
     return(
         <Box>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
             <TextField error={muiAlert && !data.nome} size='small' onChange={(e) => setData((data)=>({...data,'nome':e.target.value.toUpperCase()}))} value={data.nome} style={{margin:'1%', marginBottom: 0}} variant="outlined" label='Nome' className='nome' id='nome'></TextField>
             <TextField error={muiAlert && !data.sexo} size='small' onChange={(e) => setData((data)=>({...data,'sexo':e.target.value}))} value={data.sexo} select style={{margin:'1%', marginBottom: 0}} variant="outlined" label='Sexo' className='sexo' id='Sexo'><MenuItem value={1}>M</MenuItem><MenuItem value={2}>F</MenuItem></TextField>
             <InputMask mask="999.999.999-99" maskChar={''} onChange={(e) => setData((data)=>({...data,'cpf':e.target.value}))} value={data.cpf} id='cpf' >{() => <TextField error={muiAlert && !data.cpf} size='small' style={{margin:'1%', marginBottom: 0}} label='CPF' className='inputs'/>}</InputMask>
             <InputMask mask="999999999999999" maskChar={''} onChange={(e) => setData((data)=>({...data,'rg':e.target.value}))} value={data.rg} id='Identidade'>{() => <TextField error={muiAlert && !data.rg} size='small' style={{margin:'1%', marginBottom: 0}} label='Identidade' className='inputs'  />}</InputMask>
             <TextField error={muiAlert && !data.orgaoExpedidor} size='small' onChange={(e) => setData((data)=>({...data,'orgaoExpedidor':e.target.value.toUpperCase()}))} value={data.orgaoExpedidor} style={{margin:'1%', marginBottom: 0}} variant="outlined" label='Órgão Expedidor' className='inputs' id='orgaoExpedidor'></TextField>
-            <InputMask mask='dD/mM/YYYY' maskChar={''} formatChars={formatCharsYear} onChange={(e) => setData((data)=>({...data,'dataNascimento':e.target.value.toUpperCase()}))} value={data.dataNascimento} id='dataNascimento'>{() => <TextField error={muiAlert && !data.dataNascimento} size='small' label='Data Nascimento' className='inputs' style={{margin:'1%', marginBottom: 0}} />}</InputMask>
+            <DesktopDatePicker
+            size='small'
+            label="Data de nascimento"
+            inputFormat="dd/MM/yyyy"
+            value={data.dataNascimento}
+            onChange={(e)=>setData((data)=>({...data, dataNascimento: e}))}
+            renderInput={(params) => <TextField {...params} className='inputs' style={{margin:'1%', marginBottom:0}} size='small' />}
+            />
             <TextField error={muiAlert && !data.naturalidade} size='small' onChange={(e) => setData((data)=>({...data,'naturalidade':e.target.value.toUpperCase()}))} value={data.naturalidade} style={{margin:'1%', marginBottom: 0}} variant="outlined" label='Naturalidade' className='inputs' id='naturalidade'></TextField>
             <TextField error={muiAlert && !data.ufNaturalidade} size='small' onChange={(e) => setData((data)=>({...data,'ufNaturalidade':e.target.value}))} value={data.ufNaturalidade} select style={{margin:'1%', marginBottom: 0}} variant="outlined" label="UF Naturalidade" className='inputs' id='ufNaturalidade'>
             {uf.map((item)=><MenuItem value={item.value}>{item.txt}</MenuItem>)}</TextField>
@@ -42,6 +53,7 @@ export default function RegForm1({data, setData, muiAlert}){
             <TextField error={muiAlert && !data.pai} size='small' onChange={(e) => setData((data)=>({...data,'pai':e.target.value.toUpperCase()}))} value={data.pai} style={{margin:'1%', marginBottom: 0}} variant="outlined" label='Pai' className='pai' id='pai'></TextField>  
             <TextField error={muiAlert && !data.mae} size='small' onChange={(e) => setData((data)=>({...data,'mae':e.target.value.toUpperCase()}))} value={data.mae} style={{margin:'1%'}} variant="outlined" label='Mãe' className='mae' id='mae'></TextField>
             <TextField error={muiAlert && !data.email} size='small' type='email' onChange={(e) => setData((data)=>({...data, 'email':e.target.value.toUpperCase()}))} value={data.email} placeholder="email@example.com" style={{margin:'1%'}} variant="outlined" label='Email' className='email' id='email'></TextField>  
+            </LocalizationProvider>
         </Box>
     )
 }
