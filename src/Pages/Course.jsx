3fd84df49f64f17750fe07159d7fd500a4e211da
vctg1 <victormourago@gmail.com>
2022-07-18 18:11:42 -0300

    'create-courses-form'

--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -1,14 +1,9 @@
-import { Grid, Table, TableContainer, TextField, TableBody, TableCell, Box, Typography, TableRow, TableHead } from "@mui/material";
+import { Grid, TableContainer, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { ptBR } from 'date-fns/locale';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import CoursesForm from "../Components/Courses/CoursesForm";
 
 export default function Course(){
     let [data, setData] = useState({})
-    let API = process.env.REACT_APP_API_KEY
     let [matricula, setMatricula] = useState(JSON.parse(window.localStorage.getItem('matricula')))
     useEffect(()=>{
         setData((data)=>({
@@ -45,140 +40,25 @@ export default function Course(){
             'Observações':matricula.observacoes
         }))
     },[matricula])
-    console.log(data);
     return(
         <Grid style={{backgroundColor:'white', borderRadius:'10px'}}>
             <TableContainer>
-                <Table> 
-                    <TableRow>
-                        <Typography color={'black'} fontWeight='bold' padding={'2%'} paddingBottom='0' >DADOS DO CURSO</Typography>
-                    </TableRow>
-                        <Grid display={'grid'} gridTemplateColumns='1fr 1fr 1fr 1fr 1fr' padding={'2%'} >
-                        {Object.keys(data).slice(0,11).map((item)=> {
-                            if(data[item] instanceof Date){
-                                return(
-                                    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR} >
-                                    <DesktopDatePicker
-                                    label={item}
-                                    inputFormat="dd/MM/yyyy"
-                                    value={data[item]}
-                                    onChange={(e)=>setData((data)=>({...data, item: e}))}
-                                    renderInput={(params) => <TextField {...params} 
-                                    InputLabelProps={{
-                                        style: { fontWeight: 'bold'},
-                                    }} 
-                                    style={{margin:'2%'}} size='small' />}
-                                    />
-                                    </LocalizationProvider>
-                                )
-                            }
-                            else{
-                                return(
-                                <TextField
-                                InputLabelProps={{
-                                    style: { fontWeight: 'bold'},
-                                }}
-                                style={{margin:'2%'}}
-                                size='small'
-                                value={data[item]}
-                                label={item}
-                                onChange={(e)=>setData((data)=>({...data, [item]: e.target.value}))} 
-                                />
-                                )
-                            }                    
-                        })}
-                        </Grid>
-                        <TableRow>
-                        <Typography color={'black'} fontWeight='bold' paddingLeft={'2%'}>FINANCEIRO</Typography>
-                        </TableRow>
-                        <Grid display={'grid'} gridTemplateColumns='1fr 1fr 1fr 1fr 1fr' padding={'2%'} >
-                        {Object.keys(data).slice(12,17).map((item)=> {
-                            if(data[item] instanceof Date){
-                                return(
-                                    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR} >
-                                    <DesktopDatePicker
-                                    label={item}
-                                    inputFormat="dd/MM/yyyy"
-                                    value={data[item]}
-                                    onChange={(e)=>setData((data)=>({...data, item: e}))}
-                                    renderInput={(params) => <TextField {...params} 
-                                    InputLabelProps={{
-                                        style: { fontWeight: 'bold'},
-                                    }}
-                                    style={{margin:'2%'}} size='small' />}
-                                    />
-                                    </LocalizationProvider>
-                                )
-                            }
-                            else{
-                                return(
-                                <TextField
-                                InputLabelProps={{
-                                    style: { fontWeight: 'bold'},
-                                }}
-                                style={{margin:'2%'}}
-                                size='small'
-                                value={data[item]}
-                                label={item}
-                                onChange={(e)=>setData((data)=>({...data, [item]: e.target.value}))} 
-                                />
-                                )
-                            }                    
-                        })}
-                        </Grid>
-                        <TableRow>
-                        <Typography color={'black'} fontWeight='bold' paddingLeft={'2%'}>PROCEDIMENTOS GERAIS</Typography>
-                        </TableRow>
-                        <Grid display={'grid'} gridTemplateColumns='1fr 1fr 1fr 1fr 1fr' padding={'2%'} >
-                        {Object.keys(data).slice(18,30).map((item)=> {
-                            if(data[item] instanceof Date){
-                                return(
-                                    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR} >
-                                    <DesktopDatePicker
-                                    label={item}
-                                    inputFormat="dd/MM/yyyy"
-                                    value={data[item]}
-                                    onChange={(e)=>setData((data)=>({...data, item: e}))}
-                                    renderInput={(params) => <TextField {...params} 
-                                    InputLabelProps={{
-                                        style: { fontWeight: 'bold'},
-                                    }}
-                                    style={{margin:'2%'}} size='small' />}
-                                    />
-                                    </LocalizationProvider>
-                                )
-                            }
-                            else{
-                                return(
-                                <TextField
-                                InputLabelProps={{
-                                    style: { fontWeight: 'bold'},
-                                }}
-                                style={{margin:'2%'}}
-                                size='small'
-                                value={data[item]}
-                                label={item}
-                                onChange={(e)=>setData((data)=>({...data, [item]: e.target.value}))} 
-                                />
-                                )
-                            }                    
-                        })}
-                        </Grid>
-                        <Grid padding={'2%'} width='100%'>
-                        <TextField
-                        InputLabelProps={{
-                            style: { fontWeight: 'bold'},
-                        }}
-                        fullWidth
-                        size='small'
-                        multiline
-                        rows={4}
-                        value={data.Observações}
-                        label={'Observações'}
-                        onChange={(e)=>setData((data)=>({...data, 'Observações': e.target.value}))} 
-                        />
-                        </Grid>
-                </Table>
+                <CoursesForm data={data} setData={setData} title={'DADOS DO CURSO'} start={0} finish={11} />
+                <CoursesForm data={data} setData={setData} title={'PROCEDIMENTOS GERAIS'} start={17} finish={31} />
+                <Grid padding={'2%'} width='100%'>
+                <TextField
+                InputLabelProps={{
+                    style: { fontWeight: 'bold'},
+                }}
+                fullWidth
+                size='small'
+                multiline
+                rows={4}
+                value={data.Observações}
+                label={'Observações'}
+                onChange={(e)=>setData((data)=>({...data, 'Observações': e.target.value}))} 
+                />
+                </Grid>
             </TableContainer>
         </Grid>
     )
