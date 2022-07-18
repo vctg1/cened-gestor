36a09a98485fd6e527610a7af795529f7979a2a2
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-07-18 16:57:54 -0300

    permissionGroup v1 concluded

--- src/Pages/PermissionGroup.jsx
+++ src/Pages/PermissionGroup.jsx
@@ -1,37 +1,369 @@
 import React from 'react'
 import { Grid } from '@mui/material'
-import { BiCheckbox, BiCheckSquare } from 'react-icons/bi'
-import useMediaQuery from '@mui/material/useMediaQuery';
+import CardPemissions from '../Components/CardPemissions';
+import {AiFillLock} from 'react-icons/ai'
+import { useState } from 'react';
+import SearchInput from '../Components/input-fields/SearchInput';
 
+const groups = [
+    {
+        title: 'Home',
+        lines: [
+            {
+                title: 'Alertas do Dia',
+                consult: true,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Alunos e Cursos',
+                consult: false,
+                edit: false,
+                delete: false
+            }
+        ]
+    },
+    {
+        title: 'Configurações',
+        lines: [
+            {
+                title: 'Autorizar Curso por UF',
+                consult: true,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Token de Liberação',
+                consult: true,
+                edit: false,
+                delete: false
+            }
+        ]
+    },
+    
+    {
+        title: 'Relatórios',
+        lines: [
+            {
+                title: 'Transações Financeiras',
+                consult: true,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Cursos Sintético',
+                consult: true,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Cursos Analítico',
+                consult: true,
+                edit: false,
+                delete: false
+            }
+        ]
+    },
+    {
+        title: 'Notas Fiscais',
+        lines: [
+            {
+                title: 'Emitir Notas Fiscais',
+                consult: true,
+                edit: false,
+                delete: false
+            },
+        ]
+    },
+    {
+        title: 'Mensagens',
+        lines: [
+            {
+                title: 'Enviar Mensagens',
+                consult: true,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Cursos Sintético',
+                consult: true,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Cursos Analítico',
+                consult: true,
+                edit: false,
+                delete: false
+            }
+        ]
+    },
+
+    {
+        title: 'Rotinas Automáticas',
+        lines: [
+            {
+                title: 'Envio de Material UFs',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Envio de Material DF',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+            
+            {
+                title: 'Gerar Etiquetas',
+                consult: false,
+                edit: false,
+                delete: false  
+            },
+
+            {
+                title: 'Emitir Certificado',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Serviços Adicionais',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Matrículas Pendentes',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Código de Rastreio',
+                consult: false,
+                edit: false,
+                delete: false
+            }
+        ]
+    },
+
+    {
+        title: 'Documentos',
+        lines: [
+            {
+                title: 'Ofício DF',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Ofício UFs',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+            
+            {
+                title: 'Gerar Etiquetas',
+                consult: false,
+                edit: false,
+                delete: false  
+            },
+
+            {
+                title: 'Atas de Provas',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Protocolo de Certificado',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Protocolo de Material',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Ficha de Matrícula',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Declaração',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Resultado de Provas',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Emitir Certificado',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Livro digital de Certifcados',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Autorizações de Estudos',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+        ]
+    },
+
+    {
+        title: 'Cadastros',
+        lines: [
+            {
+                title: 'Alunos e Cursos',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'CH Diária LEP',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+            
+            {
+                title: 'Cursos',
+                consult: false,
+                edit: false,
+                delete: false  
+            },
+
+            {
+                title: 'Fiscais de Sala',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Penitencíarias',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Representantes',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Taxa Entrega/Frete',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Grupo de Permisões',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Usuários do Sistema',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Grupos de Provas',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Agentes Penitenciários',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Etiquetas de Material',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+
+            {
+                title: 'Cursos autorizados UF',
+                consult: false,
+                edit: false,
+                delete: false
+            },
+        ]
+    },
+
+]
 export default function PermissionGroup() {
-    let isDesktop = useMediaQuery('(min-width:800px)')
+    const [searchValue, setSearchValue] = useState('')
   return (
     <div className='flex justify-center p-4'>
         <Grid container spacing={2}>
-            <Grid item xs={12} className='p-2' borderRadius={'12px'} md={6} bgcolor={'#ffff'}>
-                <h2 className='text-center font-bold text-2xl'>Home</h2>
-                <div className='flex flex-col'>
-                    <div className='border-2 border-black flex justify-between p-2 rounded-xl'>
-                        <h3 className='font-semibold text-xl'>Alertas do Dia</h3>
-                        <div className='flex items-center'>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Consultar</span>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Editar</span>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Excluir</span>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Todos</span>
-                        </div>
-                    </div>
-
-                    <div className='border-2 border-black mt-2 flex justify-between p-2 rounded-xl'>
-                        <h3 className='font-semibold text-xl'>Alunos e Cursos</h3>
-                        <div className='flex items-center'>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Consultar</span>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Editar</span>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Excluir</span>
-                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Todos</span>
-                        </div>
-                    </div>
-                </div>
+            <Grid className='flex items-center' item xs={20} md={12}>
+                <AiFillLock size={30}/>
+                <h1 className='text-xl font-bold'>Grupo de Permissões</h1>
             </Grid>
+            <Grid item xs={12} md={8}>
+                <SearchInput className='w-full' placeholder='Usuário'/>
+            </Grid>
+            {
+                groups.map((perm, key)=>{
+                    return(
+                        <CardPemissions key={key} permission={perm}/>
+                    )
+                })
+            }
         </Grid>
     </div>
   )
