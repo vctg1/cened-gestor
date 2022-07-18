3019d1d29ae75150dd63da52806035b12f9eb7cf
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-07-18 15:33:35 -0300

    select all add

--- src/Pages/PermissionGroup.jsx
+++ src/Pages/PermissionGroup.jsx
@@ -6,7 +6,7 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 export default function PermissionGroup() {
     let isDesktop = useMediaQuery('(min-width:800px)')
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center p-4'>
         <Grid container spacing={2}>
             <Grid item xs={12} className='p-2' borderRadius={'12px'} md={6} bgcolor={'#ffff'}>
                 <h2 className='text-center font-bold text-2xl'>Home</h2>
@@ -17,15 +17,17 @@ export default function PermissionGroup() {
                             <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Consultar</span>
                             <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Editar</span>
                             <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Excluir</span>
+                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Todos</span>
                         </div>
                     </div>
 
                     <div className='border-2 border-black mt-2 flex justify-between p-2 rounded-xl'>
-                        <h3 className='font-semibold text-xl'>Livro digital de certificados</h3>
+                        <h3 className='font-semibold text-xl'>Alunos e Cursos</h3>
                         <div className='flex items-center'>
                             <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Consultar</span>
                             <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Editar</span>
                             <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Excluir</span>
+                            <span className='flex items-center mr-4 text-lg'><BiCheckbox size={25}/>Todos</span>
                         </div>
                     </div>
                 </div>
